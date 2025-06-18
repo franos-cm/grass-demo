c81e96e7d591bb1dab6348b30ef39157c256a4fa
Alessandro Li <alessandro_li22@hotmail.com>
2025-06-18 00:06:41 -0300

    Adds a simple grass spawner

---
+++ scripts/grass_spawner.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@export var hterrain: Node3D  # Arraste aqui o seu HTerrain
+@export var grass_scene: PackedScene  # Arraste aqui a cena da grama (grass_scene.tscn)
+@export var density: int = 1000  # Quantidade de gramas
+@export var area_min: Vector2 = Vector2(0, 0)  # Limite mínimo (x,z)
+@export var area_max: Vector2 = Vector2(512, 512)  # Limite máximo (x,z)
+@export var max_height: float = 30.0  # Altura limite para a grama aparecer
+
+func _ready():
+	randomize()
+	for i in range(density):
+		var x = randf_range(area_min.x, area_max.x)
+		var z = randf_range(area_min.y, area_max.y)
+		var y = hterrain.get_height(x, z)
+		
+		if y > max_height:
+			continue  # Não colocar grama em locais muito altos (ex: montanhas)
+
+		var grass_instance = grass_scene.instantiate()
+		grass_instance.global_position = Vector3(x, y, z)
+		grass_instance.rotation.y = randf_range(0, TAU)
+		add_child(grass_instance)
